8bb311572fe0934651a351c84077a2fa8f8d5f4b
half-frozen <m.rendondemelo@gmail.com>
2021-06-29 10:31:53 -0400

    modificando los enlaces de inicio

--- levegane/levegane/urls.py
+++ levegane/levegane/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('vegane.urls')),
+    path('inicio/', include('vegane.urls')),
     path('recetas/', include('vegane.urls')),
     path('registrar/', include('vegane.urls')),
     path('noticias/', include('vegane.urls')),
