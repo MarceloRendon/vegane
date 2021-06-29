0ceca3b0654590e13405004b201d31fe28ab8a7e
half-frozen <m.rendondemelo@gmail.com>
2021-06-29 10:21:43 -0400

    le vegane a django

---
+++ levegane/vegane/urls.py
@@ -0,0 +1,14 @@
+
+from django.urls import path
+
+from .views import home, receta, registrar, noticia, sesion, tip, lugar
+
+urlpatterns = [
+    path('', home, name="home"),
+    path('recetas/', receta, name ="receta"),
+    path('registrar/', registrar, name = "registrar"),
+    path('noticias/', noticia, name ="noticia"),
+    path('iniciar-sesion/', sesion, name ="sesion"),
+    path('tips/', tip, name ="tip"),
+    path('lugares/', lugar, name ="lugar"),
+]
---
+++ levegane/vegane/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'vegane/index.html')
+
+def registrar(request):
+    return render(request, 'vegane/registrar.html')
+
+def receta(request):
+    return render(request, 'vegane/recetas.html')
+
+def noticia(request):
+    return render(request, 'vegane/noticias.html')
+
+def sesion(request):
+    return render(request, 'vegane/iniciar-sesion.html')
+
+def tip(request):
+    return render(request, 'vegane/tips.html')
+
+def lugar(request):
+    return render(request, 'vegane/lugares.html')
